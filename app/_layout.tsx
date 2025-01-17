dd0f8aed62101e5573d5740a3a0d658d7dfa300c
ibrahim10sy <ibrahim10sygmail.com>
2025-01-17 12:11:17 +0000

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,7 @@
+import { QueryClient } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return <Stack 
