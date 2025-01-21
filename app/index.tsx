c8efd7b04b34d7f3e47dc2df25e74e36df99c437
ibrahim10sy <ibrahim10sygmail.com>
2025-01-21 15:19:25 +0000

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -40,7 +40,7 @@ export default function Index() {
     >
       <Row style={styles.header} gap={16}>
           <Image source={require("@/assets/images/pokeball.png")} width={24} height={24}/>
-          <ThemedText variant="headline" color="grayLight">Madou Soumbounou</ThemedText>
+          <ThemedText variant="headline" color="grayLight">Pokédex</ThemedText>
           {/* <Image  source={require("@/assets/images/pokeball.png")} width={24} height={24}/> */}
       </Row>  
       <Row gap={16} style={{paddingHorizontal:12}}>
